de61ec233a870f62adaa5320793057b1b7238ac4
Kieran O'Brien <hello@kieranobrien.dev>
2025-05-12 16:06:58 +0100

    Add test social link to icon

--- src/SocialIcons.jsx
+++ src/SocialIcons.jsx
@@ -2,7 +2,9 @@ function SocialIcons() {
   return (
     <>
       <div className="my-5">
-        <i className="ri-github-fill social-icon text-5xl m-3 text-shadow-md/50 hover:text-shadow-lg/90 transition-shadow duration-300 hover:text-gray-300 active:text-gray-500"></i>
+        {/* Update link visited colour */}
+        {/* Make links open new tab */}
+        <a href="https://github.com/kieran-obrien"><i className="ri-github-fill social-icon text-5xl m-3 text-shadow-md/50 hover:text-shadow-lg/90 transition-shadow duration-300 hover:text-gray-300 active:text-gray-500"></i></a>
         <i className="ri-linkedin-box-fill social-icon text-5xl m-3 text-shadow-md/50 hover:text-shadow-lg/90 transition-shadow duration-300 hover:text-gray-300 active:text-gray-500"></i>
         <i className="ri-mail-line social-icon text-5xl m-3 text-shadow-md/50 hover:text-shadow-lg/90 transition-shadow duration-300 hover:text-gray-300 active:text-gray-500"></i>
       </div>
