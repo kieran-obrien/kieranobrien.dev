99910877eadc5cdd3034c98ef661da55f84388df
Kieran O'Brien <hello@kieranobrien.dev>
2025-05-16 16:29:38 +0100

    Expand projects section, add placeholder text and skill icons, flex/grid structure

--- src/content/ProjectCard.jsx
+++ src/content/ProjectCard.jsx
@@ -1,7 +1,20 @@
 const ProjectCard = ({ title, skills, color }) => (
   <div className="flex flex-col justify-center items-center bg-muted rounded-2xl shadow-2xl/60 p-5 hover:shadow-2xl/90 transition-all group-hover:blur-xs hover:!blur-none hover:scale-110 transition duration-150">
     <img src="./placeholder.png" className="w-md h-md rounded-lg"></img>
-    <h3 className={`w-full text-center text-xl font-semibold mb-3 p-3`}>{title}</h3>
+    <h3 className={`w-full text-center text-xl font-semibold mb-3 p-3`}>
+      {title}
+    </h3>
+    <div className="flex flex-wrap justify-center items-center gap-2">
+      {skills.map((skill, index) => (
+        <span key={index}>{skill}</span>
+      ))}
+    </div>
+    <p className="text-left p-4">
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat."
+    </p>
   </div>
 );
 
--- src/content/ProjectsSection.jsx
+++ src/content/ProjectsSection.jsx
@@ -1,6 +1,8 @@
 import ProjectCard from "./ProjectCard";
 import { langs, frontend, backend, tools, other } from "./Skills";
 
+import { project1 } from "./ProjectSkills";
+
 const ProjectsSection = () => {
   return (
     <section id="projects" className="py-20 px-4 max-w-6xl mx-auto">
@@ -8,7 +10,7 @@ const ProjectsSection = () => {
         Projects
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-8 text-[var(--fg)] group">
-        <ProjectCard title="Project 1" skills={langs} />
+        <ProjectCard title="Project 1" skills={project1} />
         <ProjectCard title="Project 2" skills={frontend} />
       </div>
     </section>
