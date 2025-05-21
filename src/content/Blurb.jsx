86e479c5a86a884c2ab1c0d786ddb359d4aebfe5
Kieran O'Brien <hello@kieranobrien.dev>
2025-05-21 16:16:14 +0100

    Update blurb

--- src/content/Blurb.jsx
+++ src/content/Blurb.jsx
@@ -3,13 +3,12 @@ function Blurb({ invisible, border }) {
     <p
       className={`block sm:m-16 leading-relaxed text-lg text-center ${invisible} ${border} text-shadow-md/20 text-[var(--fg)]`}
     >
-      Hello, world! I’m Kieran — a junior software developer with a passion for building
-      clean, functional, and user-focused applications. After over a decade in a
+      Hello, world! I’m Kieran — a junior software developer with a passion for building functional and user-focused applications. After over a decade of professional experience in a
       different field, I’ve leapt headfirst into tech, bringing with me a sharp
       eye for detail, strong problem-solving skills, and a relentlessly curious
       drive to learn. Whether it’s working with JavaScript, Python, or exploring
-      the world of DevOps and Linux, I’m all about writing code that solves real
-      problems and grows with users. Let’s build something great together.
+      the world of Linux and DevOps, I’m all about writing clean code that solves real world
+      problems. Let’s build something great together!
     </p>
   );
 }
