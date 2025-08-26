071254f3f98aa18454700163544d89e3dceb97bd
Kieran O'Brien <hello@kieranobrien.dev>
2025-08-26 16:03:06 +0100

    Add alignment/wrapping to studies section, and switch from divs to <p>'s

--- src/content/studies-section/StudiesSection.jsx
+++ src/content/studies-section/StudiesSection.jsx
@@ -1,27 +1,21 @@
 export default function StudiesSection() {
   return (
-    <div className="mt-10 flex flex-col gap-1">
-      <div className="divider divider-end before:bg-transparent text-xl">
+    <div className="mt-10 flex flex-col gap-4">
+      <p className="text-xl text-wrap text-right">
         northcoders - software development with javascript bootcamp
-      </div>
-      <div className="divider divider-end text-xl before:bg-transparent after:bg-transparent">
+      </p>
+      <p className="text-xl text-wrap text-right">
         comptia - linux+ (in progress)
-      </div>
-      <div className="divider divider-end text-xl before:bg-transparent after:bg-transparent">
+      </p>
+      <p className="text-xl text-wrap text-right">
         aws - cloud practitioner (in progress)
-      </div>
-      <div className="divider divider-end text-xl before:bg-transparent after:bg-transparent">
-        havard/edx - cs50x
-      </div>
-      <div className="divider divider-end text-xl before:bg-transparent after:bg-transparent">
-        havard/edx - cs50p
-      </div>
-      <div className="divider divider-end text-xl before:bg-transparent after:bg-transparent">
+      </p>
+      <p className="text-xl text-wrap text-right">havard/edx - cs50x</p>
+      <p className="text-xl text-wrap text-right">havard/edx - cs50p</p>
+      <p className="text-xl text-wrap text-right">
         mimo - front end developer path
-      </div>
-      <div className="divider divider-end text-xl before:bg-transparent after:bg-transparent">
-        mimo - javascript
-      </div>
+      </p>
+      <p className="text-xl text-wrap text-right">mimo - javascript</p>
     </div>
   );
 }
